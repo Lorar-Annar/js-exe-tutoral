711305bd5d8d14ff6d99fa8b9a8ed7c09f380411
Lorar-Annar <lorarkhine@outlook.com>
2024-08-21 05:32:17 +0000

    datatype

---
+++ 01_basics/01_varial.js
@@ -0,0 +1,17 @@
+const accoutID = "143396"
+let accoutEmail = "Akhine@gmail.com"
+var accoutPassword = "10101010"
+accoutCity = "Sittwe"
+let accoutState;
+
+// accoutID = "155587" //this not allow
+
+console.log(accoutID);
+
+accoutEmail = "Kc@Kc.com"
+accoutPassword = "12121212"
+accoutCity = "KyaukTaw"
+
+console.table([accoutID, accoutEmail, accoutCity, accoutState]);
+
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+
+"use Strict" // treat all js code and answer version
+
+//alert( 3 + 3 ) //we are using nodejs , not browser
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state; 
+
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); //object
+
+
