d60a641f2018ae5e139781129e49bb4abf32f475
Lorar-Annar <lorarkhine@outlook.com>
2024-08-26 15:11:34 +0000

    datatype_summery

---
+++ 01_basics/datatype_summery.js
@@ -0,0 +1,28 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLonggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 12974596844564585413289n
+
+const heros = ["kyaw kyaw", "Aye Aye" , "Mg Mg"]
+
+let myObj = {
+    name:"@ayekhine",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof scoreValue);
+
